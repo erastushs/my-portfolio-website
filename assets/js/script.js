26866b4e083d115c821df9e4fa3cfca845803839
Erastus-HS <erastushs@gmail.com>
2022-06-18 21:39:39 +0900

    menambahkan scroll spy

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,35 @@ menuToggle.addEventListener("click", function () {
 $(document).on("click", "ul li", function () {
   $(this).addClass("active").siblings().removeClass("active");
 });
+
+// scroll spy
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("ul li a");
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("ul li a[href*=" + id + "]").classList.add("active");
+      });
+    }
+  });
+};
+
+// smooth scrolling
+$('a[href*="#"]').on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($(this).attr("href")).offset().top,
+    },
+    500,
+    "linear"
+  );
+});
