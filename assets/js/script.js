57621241c4d068bfbf30b8a40717e9a47cd8a8b1
Erastus-HS <erastushs@gmail.com>
2022-06-18 19:24:11 +0900

    .

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// script hamburger untuk mobile responsive
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
