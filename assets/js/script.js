29fdc1dd5128e77fc04ab4021c38c52e56a4f384
Erastus-HS <erastushs@gmail.com>
2022-07-03 22:49:44 +0900

    menghapus journey tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,31 +62,3 @@ var typed = new Typed(".typing-text", {
   backSpeed: 25,
   backDelay: 500,
 });
-
-// Journey Tabs
-let eduBtn = document.querySelector(".edu-btn"),
-  workBtn = document.querySelector(".work-btn");
-
-eduBtn.addEventListener("click", function () {
-  eduBtn.classList.add("active");
-  workBtn.classList.remove("active");
-  document.querySelector(".edu-space").classList.remove("hidden");
-  document.querySelector(".work-space").classList.add("hidden");
-});
-
-workBtn.addEventListener("click", function () {
-  workBtn.classList.add("active");
-  eduBtn.classList.remove("active");
-  document.querySelector(".work-space").classList.remove("hidden");
-  document.querySelector(".edu-space").classList.add("hidden");
-});
-
-let offset = 0;
-window.addEventListener("scroll", function () {
-  let st = window.pageYOffset;
-  if (st > offset) {
-    document.querySelector(".fa-arrow-up").classList.add("active");
-  } else {
-    document.querySelector(".fa-arrow-up").classList.remove("active");
-  }
-});
