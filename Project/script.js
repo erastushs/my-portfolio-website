af9c5e1d1a5f06c666cb21ea49ea0d17928a23f6
Erastus-HS <erastushs@gmail.com>
2022-07-03 22:43:39 +0900

    menambahkan laman project dan update beberapa fitur

--- Project/script.js
+++ Project/script.js
@@ -1,7 +1,7 @@
 // Favicon
 document.addEventListener("visibilitychange", function () {
   if (document.visibilityState === "visible") {
-    document.title = "Portfolio | Erastus HS";
+    document.title = "Project | Portfolio Erastus HS";
     $("#favicon").attr("href", "assets/img/foto/logo.png");
   } else {
     document.title = "Welcome to My Portfolio";
@@ -29,7 +29,7 @@ let navLinks = document.querySelectorAll("ul li a");
 window.onscroll = () => {
   section.forEach((sec) => {
     let top = window.scrollY;
-    let offset = sec.offsetTop - 250;
+    let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
@@ -47,46 +47,9 @@ $('a[href*="#"]').on("click", function (e) {
   e.preventDefault();
   $("html, body").animate(
     {
-      scrollTop: $($(this).attr("href")).offset().top - 70,
+      scrollTop: $($(this).attr("href")).offset().top,
     },
     500,
     "linear"
   );
-});
-
-// <!-- typed js effect starts -->
-var typed = new Typed(".typing-text", {
-  strings: ["frontend development", "web development"],
-  loop: true,
-  typeSpeed: 50,
-  backSpeed: 25,
-  backDelay: 500,
-});
-
-// Journey Tabs
-let eduBtn = document.querySelector(".edu-btn"),
-  workBtn = document.querySelector(".work-btn");
-
-eduBtn.addEventListener("click", function () {
-  eduBtn.classList.add("active");
-  workBtn.classList.remove("active");
-  document.querySelector(".edu-space").classList.remove("hidden");
-  document.querySelector(".work-space").classList.add("hidden");
-});
-
-workBtn.addEventListener("click", function () {
-  workBtn.classList.add("active");
-  eduBtn.classList.remove("active");
-  document.querySelector(".work-space").classList.remove("hidden");
-  document.querySelector(".edu-space").classList.add("hidden");
-});
-
-let offset = 0;
-window.addEventListener("scroll", function () {
-  let st = window.pageYOffset;
-  if (st > offset) {
-    document.querySelector(".fa-arrow-up").classList.add("active");
-  } else {
-    document.querySelector(".fa-arrow-up").classList.remove("active");
-  }
 });
