2ad4d9e1909d0e5b8626ef84bd49760e63ec09ba
Erastus-HS <erastushs@gmail.com>
2022-07-21 19:38:38 +0900

    perbaiki project

--- Project/script.js
+++ Project/script.js
@@ -2,10 +2,10 @@
 document.addEventListener("visibilitychange", function () {
   if (document.visibilityState === "visible") {
     document.title = "Project | Portfolio Erastus HS";
-    $("#favicon").attr("href", "assets/img/foto/logo.png");
+    $("#favicon").attr("href", "/assets/img/foto/logo.png");
   } else {
     document.title = "Welcome to My Portfolio";
-    $("#favicon").attr("href", "assets/img/foto/favicon.png");
+    $("#favicon").attr("href", "/assets/img/foto/favicon.png");
   }
 });
 
@@ -15,41 +15,4 @@ const nav = document.querySelector("nav ul");
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
-});
-
-//script toggle navbar aktif
-$(document).on("click", "ul li", function () {
-  $(this).addClass("active").siblings().removeClass("active");
-});
-
-// scroll spy
-let section = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("ul li a");
-
-window.onscroll = () => {
-  section.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document.querySelector("ul li a[href*=" + id + "]").classList.add("active");
-      });
-    }
-  });
-};
-
-// smooth scrolling
-$('a[href*="#"]').on("click", function (e) {
-  e.preventDefault();
-  $("html, body").animate(
-    {
-      scrollTop: $($(this).attr("href")).offset().top,
-    },
-    500,
-    "linear"
-  );
 });
