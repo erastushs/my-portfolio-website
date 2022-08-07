6485ec4ea9e4a1abce43f98a1233812827cb3f5d
Erastus-HS <erastushs@gmail.com>
2022-08-08 02:45:50 +0900

    mengubah auto typed ke vanilla javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,14 +54,65 @@ $('a[href*="#"]').on("click", function (e) {
   );
 });
 
-// <!-- typed js effect starts -->
-var typed = new Typed(".typing-text", {
-  strings: ["frontend development", "web development"],
-  loop: true,
-  typeSpeed: 50,
-  backSpeed: 25,
-  backDelay: 500,
-});
+// typed js vanilla
+let dataTyping = {
+  target: "typing-text",
+  text: '["frontend development", "web development"]',
+  delay: "1000",
+};
+
+class textType {
+  constructor(el, text, delay) {
+    this.text = text;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(delay, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+  }
+  tick() {
+    let i = this.loopNum % this.text.length;
+    let fullTxt = this.text[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerText = this.txt;
+
+    let that = this;
+    let timing = Math.floor(200 - Math.random() * 100);
+
+    if (this.isDeleting) {
+      timing /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      timing = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.loopNum++;
+      timing = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, timing);
+  }
+}
+
+window.onload = function () {
+  let el = document.getElementsByClassName(dataTyping.target);
+  for (let i = 0; i < el.length; i++) {
+    if (dataTyping.text) {
+      new textType(el[i], JSON.parse(dataTyping.text), dataTyping.delay);
+    }
+  }
+};
 
 // scroll up pop up
 let offset = 0;
